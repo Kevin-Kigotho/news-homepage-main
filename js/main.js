1f24c74e0fa964965e83fd5734aa55a781108b0a
kevin-kigotho <Kevin-Kigotho@users.noreply.github.com>
2023-10-27 04:18:43 +0300

    Responsive navbar, project completed
     Project completed

--- js/main.js
+++ js/main.js
@@ -1,5 +1,18 @@
+const navItems = document.querySelector('.navbar_items');
 const burger = document.querySelector('.burger');
+const body = document.querySelector('body');
 
 burger.addEventListener('click', function () {
-  document.body.classList.toggle('open');
+  const visibility = navItems.getAttribute('data-visible');
+
+  if (visibility === 'false') {
+    navItems.setAttribute('data-visible', true);
+    burger.setAttribute('aria-expanded', true);
+    burger.setAttribute('src', 'assets/images/icon-menu-close.svg');
+    body.style.backdropFilter = 'blur(5px)';
+  } else if (visibility === 'true') {
+    navItems.setAttribute('data-visible', false);
+    burger.setAttribute('aria-expanded', false);
+    burger.setAttribute('src', 'assets/images/icon-menu.svg');
+  }
 });
